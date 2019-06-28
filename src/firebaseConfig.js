8cd2974244b01894918ac287ba74f3a33dc4c953
OJPARKINSON <olliepar01@gmail.com>
2019-06-28 20:53:37 +0100

    test

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,6 @@
 export default {
     apiKey: "AIzaSyCz_mF_wnajF44yiqr27fEJNCTZsGtTLGU",
-    authDomain: "speedycars-cf405.firebaseapp.com",
+    authDomain: "https://master.d8mjoejrs9n2y.amplifyapp.com/",
     databaseURL: "https://speedycars-cf405.firebaseio.com",
     projectId: "speedycars-cf405",
     storageBucket: "",
